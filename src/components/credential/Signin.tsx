f3d3edea72ecc9653291da8dab99aed561d59282
https://github.com/Amanueltamirat <amanuel.tamirat22@gmail.com>
2025-08-27 13:10:40 +0300

    update

--- src/components/credential/Signin.tsx
+++ src/components/credential/Signin.tsx
@@ -18,12 +18,13 @@ function Signin({setIsAdmin}:boolean) {
   return (
     <div className='flex flex-col items-center gap-5 justify-center w-[30%] rounded-3xl shadow-sm  shadow-gray-500 m-auto mt-[5%] bg-white py-10 px-5'>
         <div>
-            <h2 className='flex flex-col gap-0 py-5'><span><strong className='text-indigo-900 text-3xl'>Gizat</strong><small className='text-indigo-900 text-3xl'>global</small></span> <span className='ml-[22%] -mt-1 text-sm'>Communication</span></h2>
+            {/* <h2 className='flex flex-col gap-0 py-5'><span><strong className='text-indigo-900 text-3xl'>Gizat</strong><small className='text-indigo-900 text-3xl'>global</small></span> <span className='ml-[22%] -mt-1 text-sm'>Communication</span></h2> */}
+            <img src='/logo.png' className='bg-black px-4 py-2 rounded-2xl' alt='logo'/>
         </div>
         
         <div className='flex flex-col items-center gap-5'>
             <div>
-            <p className='text-xl font-semibold'>GizatGlobal Management Portal</p>
+            <p className='text-xl font-semibold'>Voxco Number Ordering Portal</p>
             <p>Sign in to continue</p>
             </div>
 
